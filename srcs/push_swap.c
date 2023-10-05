230cd60c7a98fe6b7cfe25598d82562966bfc600
Ali Saeed <alsaeed@lab1r6s11.42abudhabi.ae>
2023-10-05 15:05:44 +0400

    movesc

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: alsaeed <alsaeed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 17:02:47 by alsaeed           #+#    #+#             */
-/*   Updated: 2023/10/05 14:44:39 by alsaeed          ###   ########.fr       */
+/*   Updated: 2023/10/05 15:04:23 by alsaeed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	**get_array(int ac, char **av)
 {
 	char	*str;
 	char	**array;
-	
+
 	str = NULL;
 	array = NULL;
 	str = ft_strjoin_sp(ac, av);
@@ -33,19 +33,14 @@ void	init_stacks(int ac, char **av, t_list *stack_a, t_list *stack_b)
 {
 	char	**array_str;
 	t_list	*curr;
-	(void) stack_b;
 	
 	array_str = get_array(ac, av);
 	stack_a = ft_make_stack(array_str);
 	ft_free_array(array_str);
 	curr = stack_a;
-	printf("stack_a:\n");
-	while (curr != NULL)
-	{
-		printf("%d\n", curr->content);
-		curr = curr->next;
-	}
-	ra(&stack_a);
+	pb(&stack_a, &stack_b);
+	pb(&stack_a, &stack_b);
+	pb(&stack_a, &stack_b);
 	curr = stack_a;
 	printf("stack_a:\n");
 	while (curr != NULL)
@@ -53,13 +48,13 @@ void	init_stacks(int ac, char **av, t_list *stack_a, t_list *stack_b)
 		printf("%d\n", curr->content);
 		curr = curr->next;
 	}
-	// printf("stack_b:\n");
-	// curr = stack_b;
-	// while (curr != NULL)
-	// {
-	// 	printf("%d\n", curr->content);
-	// 	curr = curr->next;
-	// }
+	printf("stack_b:\n");
+	curr = stack_b;
+	while (curr != NULL)
+	{
+		printf("%d\n", curr->content);
+		curr = curr->next;
+	}
 }
 
 int main (int ac, char **av)
